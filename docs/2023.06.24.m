6abd721803a6fc280394415e068c4a1b9b626816
Nieuport <millors21@gmail.com>
2023-06-24 10:17:30 +0200

    Create 2023.06.24.m

---
+++ docs/2023.06.24.m
@@ -0,0 +1,13 @@
+## Unusual review of news from the Internet
+
+# 2023.06.24
+
+[Breaking the Code - Exploiting and Examining CVE-2023-1829 in cls_tcindex Classifier Vulnerability](https://starlabs.sg/blog/2023/06-breaking-the-code-exploiting-and-examining-cve-2023-1829-in-cls_tcindex-classifier-vulnerability/)
+
+[Android Deep Link issues and WebView Exploitation | 8kSec Blogs](https://8ksec.io/android-deeplink-and-webview-exploitation-8ksec-blogs/)
+
+[Dumping the Amlogic A113X Bootrom](https://haxx.in/posts/dumping-the-amlogic-a113x-bootrom/)
+
+[Ham Radio All-in-one-Cable](https://github.com/skuep/AIOC)
+
+[Finding and utilising leaked code signing certificates](https://tij.me/blog/finding-and-utilising-leaked-code-signing-certificates/)
