ff3599a7687a669e86157809e3a065a20d33240e
Nieuport <millors21@gmail.com>
2025-11-27 10:33:46 +0100

    Create 2025.11.28.m

---
+++ docs/2025.11.28.m
@@ -0,0 +1,13 @@
+## Unusual review of news from the Internet
+
+# 2025.11.28
+
+[The minefield between syntaxes: exploiting syntax confusions in the wild](https://www.yeswehack.com/learn-bug-bounty/syntax-confusion-ambiguous-parsing-exploits)
+
+[Covert red team phishing with Phishing Club](https://phishing.club/blog/covert-red-team-phishing-with-phishing-club/)
+
+[Making Serialization Gadgets by Hand - .NET](https://www.vulncheck.com/blog/making-dotnet-gadgets)
+
+[x64 Return Address Spoofing](https://hulkops.gitbook.io/blog/red-team/x64-return-address-spoofing)
+
+[Simulating EtherHiding: Blockchain as a Malware](https://cymulate.com/blog/simulating-etherhiding-blockchain-as-a-malware/)
